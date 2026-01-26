fb27670856c1fd2da0eb19d83b9bd2c4f61684b8
tubyliec <antikruk@vivaldi.net>
2026-01-27 00:10:50 +0300

    fix: fix styles

--- image-gallery/app/index.js
+++ image-gallery/app/index.js
@@ -1,4 +1,3 @@
-import './styles.scss';
 import { HomePage } from './pages/home/home.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,17 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
         <header>
             <div class="header-container">
                 <div class="header-logo">
-                    <img class="logo-image" src="../public/assets/icons/unsplash.png" alt="Unsplash">
+                    <img class="logo-image" src="public/assets/icons/unsplash.png" alt="Unsplash">
                     <h1 class="logo-title">Unsplash API</h1>
                 </div>
                 <div class="search-container">
                     <form class="search-form">
                         <button class="button button--submit" type="submit">
-                            <img class="button__image" src="../public/assets/icons/search-icon.png" alt="search button">
+                            <img class="button__image" src="public/assets/icons/search-icon.png" alt="search button">
                         </button>
                         <input class="search" type="text" name="text" value="Belarus" placeholder="Search..." autocomplete="off" autofocus>
                         <button class="button button--clear clear-button" type="button">
-                            <img class="button__clear-image" src="../public/assets/icons/clear.png" alt="clear button">
+                            <img class="button__clear-image" src="public/assets/icons/clear.png" alt="clear button">
                         </button>
                     </form>
                 </div>
@@ -30,11 +29,11 @@ document.addEventListener('DOMContentLoaded', () => {
         </main>
         <footer>
             <a href="https://github.com/Tubyliec">
-                <img src="../public/assets/icons/github.svg" alt="GitHub">
+                <img src="public/assets/icons/github.svg" alt="GitHub">
             </a>
             <p>2024</p>
             <a href="https://rs.school/courses/javascript">
-                <img src="../public/assets/icons/rsschool.svg" alt="Rsschool Icon">
+                <img src="public/assets/icons/rsschool.svg" alt="Rsschool Icon">
             </a>
         </footer> 
     </div>
