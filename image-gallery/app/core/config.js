898f0a233d03bc7bf5fd969d56b680fdd356f077
tubyliec <antikruk@vivaldi.net>
2026-01-28 01:06:53 +0300

    chore: add vite

--- image-gallery/app/core/config.js
+++ image-gallery/app/core/config.js
@@ -10,12 +10,12 @@ export const APP_CONFIG = {
 };
 
 export const ASSETS_CONFIG = {
-  BASE_PATH: '../public',
+  BASE_PATH: '/public',
   ICONS: {
-    UNSPLASH: '../public/icons/unsplash.png',
-    SEARCH: '../public/icons/search-icon.png',
-    CLEAR: '../public/icons/clear.png',
-    GITHUB: '../public/icons/github.svg',
-    RSSCHOOL: '../public/icons/rsschool.svg',
+    UNSPLASH: '/icons/unsplash.png',
+    SEARCH: '/icons/search-icon.png',
+    CLEAR: '/icons/clear.png',
+    GITHUB: '/icons/github.svg',
+    RSSCHOOL: '/icons/rsschool.svg',
   },
 };
