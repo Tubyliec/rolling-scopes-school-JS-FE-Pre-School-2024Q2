04c16a9c8242579797bd5e16d30e186ee45a9c65
tubyliec <antikruk@vivaldi.net>
2026-01-28 01:39:37 +0300

    chore: update dist

--- image-gallery/vite.config.js
+++ image-gallery/vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'app',
-  base: '/image-gallery/',
+  base: '/rolling-scopes-school-JS-FE-Pre-School-2024Q2/image-gallery/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
