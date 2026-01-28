1558a932f885a73eb1a9e0ff82b9f6a3c123e0fc
tubyliec <antikruk@vivaldi.net>
2026-01-29 00:43:52 +0300

    chore: install vite

---
+++ audio-player/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'app',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'app/index.html'),
+      },
+      output: {
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]',
+      },
+    },
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  publicDir: '../public',
+});
